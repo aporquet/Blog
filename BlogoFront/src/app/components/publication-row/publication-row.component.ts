e5afac29697f42b9cb5850d521fc470ebc13203a
aporquet <antoine.porquet34070@gmail.com>
2019-07-29 23:16:37 +0200

    get exact number of publication-row from api

--- BlogoFront/src/app/components/publication-row/publication-row.component.ts
+++ BlogoFront/src/app/components/publication-row/publication-row.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import Publication from 'src/app/models/Publication';
 import { ApiService } from 'src/app/services/api.service';
 import { FormBuilder } from '@angular/forms';
+import { PublicationService } from 'src/app/services/publication/publication.service';
 
 @Component({
   selector: 'app-publication-row',
@@ -13,7 +14,9 @@ export class PublicationRowComponent implements OnInit {
 
   @Input() publication: Publication;
 
-  constructor(private formBuilder: FormBuilder, private apiService: ApiService) { }
+  // constructor(private formBuilder: FormBuilder, private publicationService: PublicationService) { }
+
+  constructor(private publicationService: PublicationService) { }
 
   ngOnInit() {
   }
