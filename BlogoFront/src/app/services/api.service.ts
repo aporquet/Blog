388490e1ced93f9ddcd05076ce22f9b8036e0d1d
aporquet <antoine.porquet34070@gmail.com>
2019-07-31 20:32:11 +0200

    update api service to call publication service

--- BlogoFront/src/app/services/api.service.ts
+++ BlogoFront/src/app/services/api.service.ts
@@ -1,15 +1,16 @@
 import {HttpClient} from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import Publication from '../models/Publication';
+import { PublicationService } from './publication/publication.service';
 
 export class ApiService {
 
     private url = environment.url;
 
-    constructor(private httpClient: HttpClient) {
+    constructor(private publicationService: PublicationService) {
     }
 
     firstCall() {
-        return this.httpClient.get<Publication[]>(this.url + 'publications');
+        return this.publicationService.getPublications();
     }
 }
